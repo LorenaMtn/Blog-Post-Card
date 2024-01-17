71365cf2a78bc2ce148442b691f42013ae52d21c
Lorena Munteanu <c_lorena-elena.munteanu@viasat.com>
2024-01-17 14:23:16 +0200

    first draft

---
+++ src/components/Card/card.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic cards
+ */
+@Component({
+  selector: 'card',
+  templateUrl: 'card',
+  standalone: true,
+})
+export class Card {}
